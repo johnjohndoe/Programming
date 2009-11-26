cb05a4ae5d2538d0e56e215793639be028cab24b
johnjohndoe <tobias.preuss@gmail.com>
2009-11-26 03:11:21 +0100

    Added Node class as a wrapper for the MP3Data class.

---
+++ MP3Tool/src/Node.cpp
@@ -0,0 +1,11 @@
+#include "StdAfx.h"
+#include "Node.h"
+
+
+Node::Node( void)
+: data( NULL), next( NULL), prev( NULL)
+{
+}
+Node::~Node( void)
+{
+}
---
+++ MP3Tool/src/Node.h
@@ -0,0 +1,35 @@
+#pragma once
+#if !defined ( NODE_H )
+#define NODE_H
+
+
+#include "MP3Data.h"
+
+
+/**
+ * \class	Node
+ * \brief	Wrapper to store relationship between nodes of MP3Data.
+ * \author	Tobias Preuss (s0516424) & Alexander Kramer (s0516343)
+**/
+class Node
+{
+
+public:
+
+	/// \brief Default constructor.
+	Node( void);
+	/// \brief Destructor.
+	~Node( void);
+	/// \brief Stores the metadata object.
+	MP3Data * data;
+	/// \brief Pointer to the next node.
+	Node * next;
+	/// \brief Pointer to the previous node.
+	Node * prev;
+
+private:
+
+
+};
+
+#endif;
