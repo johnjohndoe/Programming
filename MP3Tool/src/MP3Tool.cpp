91f7400986d40ceb668c7da8583018c222d6ed19
johnjohndoe <tobias.preuss@gmail.com>
2009-11-26 03:14:30 +0100

    Added temporary debugging code for NodeList.

--- MP3Tool/src/MP3Tool.cpp
+++ MP3Tool/src/MP3Tool.cpp
@@ -9,23 +9,55 @@
 
 #include "stdafx.h"
 #include "Form1.h"
+#include "MP3Data.h"
+#include "MP3DataVector.h"
+#include "Node.h"
+#include "NodeList.h"
+#include "Helper.h"
+#include <ostream>
 
 using namespace MP3Tool;
 
 [STAThreadAttribute]
 int main(array<System::String ^> ^args)
 {
+
+	// -------------------------------------------- DEBUGGING NODE LIST
+
+	NodeList * nl = new NodeList();
+	MP3Data * data1 = new MP3Data();	data1->setTitle( "alpha");
+	MP3Data * data2 = new MP3Data();	data2->setTitle( "aber davor");
+	MP3Data * data3 = new MP3Data();	data3->setTitle( "ac dazwischen");
+	MP3Data * data4 = new MP3Data();	data4->setTitle( "berta");
+	MP3Data * data5 = new MP3Data();	data5->setTitle( "anton");
+
+	nl->insert( data2);	
+	nl->insert( data1);	
+	nl->insert( data4);	
+	nl->insert( data3);
+	nl->insert( data5);
+
+	nl->print( std::ofstream("..\\data\\nodes.txt"));
+
+	const char * alpha = "anton";
+	MP3Data * result = nl->find( alpha);
+
+	// Add breakpoint here to see the node disappearing.
+	nl->remove( alpha);
+	nl->print( std::ofstream("..\\data\\nodes.txt"));
+
+
+	// -------------------------------------------- END OF DEBUGGING NODE LIST
+
+
+
 	// Enabling Windows XP visual effects before any controls are created
 	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false); 
+	Application::SetCompatibleTextRenderingDefault( false); 
 
 	// Create the main window and run it
-	Application::Run(gcnew Form1());
+	Application::Run( gcnew Form1());
 
-	// Simple library test: debug variable 'text'
-
-	ID3_Tag myTag;
-	myTag.Link("..\\data\\song.mp3");
 
 	return 0;
 
