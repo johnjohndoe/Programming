8b910210f365c140afb80a9d6b789175af2e6dc8
johnjohndoe <tobias.preuss@gmail.com>
2009-11-28 18:16:00 +0100

    Added tokenizer to split strings into words.

--- MP3Tool/src/Helper.h
+++ MP3Tool/src/Helper.h
@@ -3,6 +3,10 @@
 #define HELPER_H
 
 
+#include <string> // tokenize()
+#include <vector> // tokenize()
+
+
 /**
  * \class	Helper
  * \brief	Offers static methods to process strings.
@@ -19,6 +23,8 @@ public:
 	static unsigned int length( const char * c);
 	/// \brief Returns the lexical order of the elements in regards to the first parameter.
 	static int compareCaseSensitive( const char * lhs, const char * rhs);
+	/// \brief Splits a string into separate words and stores them in the supplied container.
+	static void tokenize( const std::string & str, std::vector<std::string> & tokens, const std::string & delimiters = " ");
 
 private:
 
