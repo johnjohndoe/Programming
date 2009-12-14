56789dc88f1fa52dd8624919d10d5eb7308ea9b7
johnjohndoe <tobias.preuss@gmail.com>
2009-12-14 16:08:06 +0100

    Changed include guards

--- MP3Tool/src/ITrackManager.h
+++ MP3Tool/src/ITrackManager.h
@@ -1,3 +1,8 @@
+#pragma once
+#if !defined ( ITRACKMANAGER_H )
+#define ITRACKMANAGER_H
+
+
 //////////////////////////////////////////////////////////////////////////////
 // Programming - Übung 3
 // Schnittstelle ITrackManager.h
@@ -9,14 +14,11 @@
 #include <string>
 using namespace std;
 
-#ifndef ITrackManager_H
-#define ITrackManager_H
-
 
 // MP3-Metadaten. 
 struct CTrackInfo
 {
-   int mIndex;  // 2009-12-07 Ergänzung TW
+   int mIndex;
    string mTitle;
    string mInterpret;
    string mAlbum;
@@ -40,20 +42,20 @@ public:
    // param: 
    // return: index, mit dem auf den Track zugegriffen werden kann
    //         INVALID_INDEX, wenn der Track bereits enthalten ist.
-   virtual int addTrack( const string pFileName, /*out*/ CTrackInfo &pTrackData ) = 0;
+   virtual int addTrack( const string pFileName, /*out*/ CTrackInfo &pTrackData) = 0;
 
    // Entfernt einen Track mit dem gegebenen Index
    // return: false, wenn der Index nicht enthalten war
-   virtual bool removeTrack( int pIndex ) = 0;
+   virtual bool removeTrack( int pIndex) = 0;
 
    // Startet eine Suche nach den Wortanfängen der Track-Titel. 
    // Es werden noch keine gefundenen Tracks zurückgeliefert, sondern nur die Anzahl der 
    // passenden Tracks, und eine Such-ID.  Mit der Such-ID können anschließend per 
    // trackGetNext() die einzelnen gefundenen Track abgerufen werden. 
    // Mit trackSearchStop() wird die aktuelle Suche beendet.
-   virtual int trackSearchStart( const string &pTitleBeginn, /*out*/ TSearchID &pID ) = 0;
-   virtual bool trackGetNext( TSearchID pID, /*out*/ CTrackInfo &pNextTrack ) = 0;
-   virtual void trackSearchStop( TSearchID pID ) = 0;
+   virtual int trackSearchStart( const string &pTitleBeginn, /*out*/ TSearchID &pID) = 0;
+   virtual bool trackGetNext( TSearchID pID, /*out*/ CTrackInfo &pNextTrack) = 0;
+   virtual void trackSearchStop( TSearchID pID) = 0;
 };
 
 #endif // ITrackManager_H
