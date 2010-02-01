6697b9809844296250ea70019b26f6e2f9f3405c
alex <alexander.kramer@htw-berlin.de>
2010-02-01 02:36:57 +0100

    set the basics for the first test.

--- MP3Tool/src/MP3Tool.cpp
+++ MP3Tool/src/MP3Tool.cpp
@@ -10,18 +10,23 @@
 
 #include <iostream>
 #include <string>
+#include "ITrackManager.h"
+#include "TrackManagerFactory.h"
 #include "stdafx.h"
 #include "boost/thread.hpp"
 #include "boost/thread/mutex.hpp"
 #include "ThreadTest.h"
+#include "TrackManager.h"
+
+#include <vector>
 
 
 boost::mutex io_mutex;
 
 
-struct count
+struct count2
 {
-  count(int id) : id(id) { }
+  count2(int id) : id(id) { }
   void operator()()
   {
     for (int i = 0; i < 10; ++i)
@@ -34,28 +39,68 @@ struct count
 };
 
 
+void addTrackWithoutThread(TrackManager * trckMngr, const std::string file)
+{
+	CTrackInfo * trackInfo = new CTrackInfo;
+	trckMngr->addTrack(file, *trackInfo);
+}
+
+void addTrackWithThread(TrackManager * trckMngr, std::string file)
+{
+	CTrackInfo * trackInfo = new CTrackInfo;
+	trckMngr->addTrack(file, *trackInfo);
+}
+
+void testOne(TrackManager * trckMngr, TrackManager * trckMngrCmpr,  vector<const std::string> * musicFiles)
+{
+	
+	std::string * t_filename = new std::string("");;
+	// Erstellen von zwei Trackmanaer
+	for( unsigned i=0; i<musicFiles->size(); i++ )
+    {
+		// Diesen Vorgang in einen Thread auslagern
+		addTrackWithThread(trckMngr, musicFiles->at(i));
+
+
+
+		// Das selbe ohne Thread in einen zweiten Manager einfügen
+		addTrackWithoutThread(trckMngrCmpr, musicFiles->at(i));
+
+		std::cout << "Hinzugefügt + "  << std::endl;
+
+	}
+	
+	// Ausgabe ob vergleich Funktioniert hat
+	if (trckMngr->isEqual(trckMngrCmpr))
+	{
+		std::cout << "Die beiden Listen sind gleich" << std::endl;
+	}
+	else
+	{
+		std::cout << "Die beiden Listen sind ungleich" << std::endl;
+	}
+}
 
 int main( int argc, char* argv[])
 {
-	/*
-	if( argc < 2)
-	{
-		std::cout << "Please enter the path as the 1. argument for the executable." << std::endl;
-		return 1;
-	}
-	*/
-	std::string dummyPath = "C:\\01_MP3Test";	
-	//ThreadTest myThreadTest( argv[ 1]);
-	ThreadTest myThreadTest( dummyPath.c_str());
-	myThreadTest.processAllTests();
+
+	vector<const std::string> * musicFiles = new vector<const std::string>;
+	musicFiles->push_back("c:\\a3b.mp3");
+	musicFiles->push_back("c:\\a3a.mp3");
+	musicFiles->push_back("c:\\a4.mp3");
+	musicFiles->push_back("c:\\a4-2.mp3");
+	
 
 
-	//boost::thread thrd1(count(0));
+	ITrackManager * trckMngr = TrackManagerFactory::getTrackManager();
+	ITrackManager * trckMngrCmpr = TrackManagerFactory::getTrackManager();
 
+	testOne((TrackManager*)trckMngr, (TrackManager*)trckMngrCmpr , musicFiles);
+
+
+	//boost::thread thrd1(count2(0));
 	//thrd1.join();
-
 
 	return 0;
 
 } // eo main
-
